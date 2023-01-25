7c61153e9410acd05876cb3995f2574666b8a9b5
Dmitry Sorokin <dimamicrosows145@yandex.ru>
2023-01-26 04:35:07 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+import MySelect from "./components/UI/select/MySelect";
+import "./styles/App.css";
+
+function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "aa", body: "юю" },
+    { id: 2, title: "dd", body: "аа" },
+    { id: 3, title: "cc", body: "яя" },
+  ]);
+  const [selectedSort, setSelectedSort] = useState("");
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
+
+  return (
+    <div className="App">
+      <PostForm create={createPost} />
+      <hr style={{ margin: "15px 0" }}></hr>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
+      </div>
+      {posts.length ? (
+        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Посты не были найдены!!!</h1>
+      )}
+    </div>
+  );
+}
+
+export default App;
